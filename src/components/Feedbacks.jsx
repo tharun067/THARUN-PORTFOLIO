279dbb13fedfa72592eeceae56d5b5ffcffa9fcb
tharun067 <tharunchava126@gmail.com>
2025-05-28 16:45:19 +0530

    few changes

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -9,10 +9,6 @@ import { testimonials } from "../constants";
 const FeedbackCard = ({
   index,
   testimonial,
-  name,
-  designation,
-  company,
-  image,
 }) => (
   <motion.div
     variants={fadeIn("", "spring", index * 0.5, 0.75)}
@@ -23,22 +19,7 @@ const FeedbackCard = ({
     <div className='mt-1'>
       <p className='text-white tracking-wider text-[18px]'>{testimonial}</p>
 
-      <div className='mt-7 flex justify-between items-center gap-1'>
-        <div className='flex-1 flex flex-col'>
-          <p className='text-white font-medium text-[16px]'>
-            <span className='blue-text-gradient'>@</span> {name}
-          </p>
-          <p className='mt-1 text-secondary text-[12px]'>
-            {designation} of {company}
-          </p>
-        </div>
-
-        <img
-          src={image}
-          alt={`feedback_by-${name}`}
-          className='w-10 h-10 rounded-full object-cover'
-        />
-      </div>
+      
     </div>
   </motion.div>
 );
